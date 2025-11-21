b5afe790a33bce32f181a1175776462ca30efcc2
12999355@itisrossi.vi.it <12999355@itisrossi.vi.it>
2025-11-21 23:15:09 +0100

    Fine

--- Barin_Attacco_Al_re/Barin_Attacco_Al_re/Program.cs
+++ Barin_Attacco_Al_re/Barin_Attacco_Al_re/Program.cs
@@ -31,7 +31,6 @@ namespace Barin_Attacco_Al_re
             {
                 if (Gioco.ListaPersonaggi[i] is CRe Re)
                 {
-                    selectedElement=i;
                     Gioco.ReAttaccato += OnReAttaccato;
                 }
                 else if (Gioco.ListaPersonaggi[i] is CGuardia Guardia)
@@ -42,17 +41,16 @@ namespace Barin_Attacco_Al_re
                 {
                     Gioco.ReAttaccato += OnReAttaccato;
                 }
-                selectedElement++;
+
             }
         }
 
         public static void OnReAttaccato(object sender, string s)
         {
-            Console.WriteLine(selectedElement);
-            if(Gioco.ListaPersonaggi[selectedElement] is CRe re)
+            if (Gioco.ListaPersonaggi[selectedElement] is CRe re)
             {
                 Console.WriteLine(re.ReAttaccato());
-                OutputFinale += "\nIl re è stato attaccato";
+                OutputFinale += $"\n{re.ReAttaccato()}";
             }
             if (Gioco.ListaPersonaggi[selectedElement] is CGuardia guardia)
             {
@@ -61,32 +59,9 @@ namespace Barin_Attacco_Al_re
             {
                 Console.WriteLine(pedone.ReAttaccato());
             }
-                
+            selectedElement = selectedElement < Gioco.ListaPersonaggi.Count - 1 ? selectedElement + 1: 0; 
         }
         
 
     }
 }
-
-/*
-        public static void OnRe_ReAttaccato(object sender, string s)
-        {
-            Console.WriteLine("Il re è stato attaccato");
-            OutputFinale += "\nIl re è stato attaccato";
-        }
-        public static void OnGuardia_ReAttaccato(object sender, string s)
-        {
-            if(Gioco.ListaPersonaggi[selectedElement] is CGuardia guardia)
-            Console.WriteLine(guardia.ReAttaccato());
-        }
-        public static void OnPedone_ReAttaccato(object sender, string s)
-        {
-            if (Gioco.ListaPersonaggi[selectedElement] is CPedone pedone)
-                Console.WriteLine(pedone.ReAttaccato());
-        }*/
-
-/*
-(s, e) =>
-{
-    Console.WriteLine(Re.ReAttaccato());
-};*/
